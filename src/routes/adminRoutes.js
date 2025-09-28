fdcf24b52948ebba1da885d903158f39befacf2a
Minh Nguyen <draculedolar0763@gmail.com>
2025-09-28 15:14:11 +0700

    first commit

---
+++ src/routes/adminRoutes.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import { userAdminController } from '../controllers/userAdminController.js'
+import { authMiddleware } from '../middlewares/authMiddleware.js'
+
+const router = express.Router()
+
+// USER
+// Lấy danh sách user
+router.get('/users', authMiddleware.authenTokenCookie, authMiddleware.isAdmin, userAdminController.getAllUsers)
+// Block user
+router.patch('/users/:id/block', authMiddleware.authenTokenCookie, authMiddleware.isAdmin, userAdminController.blockUser)
+// Unlock user
+router.patch('/users/:id/unlock', authMiddleware.authenTokenCookie, authMiddleware.isAdmin, userAdminController.unlockUser)
+// Đếm số lượng khách hàng (role customer)
+router.get('/users/count', authMiddleware.authenTokenCookie, authMiddleware.isAdmin, userAdminController.countCustomers)
+
+export default router
---
+++ src/server.js
@@ -0,0 +1,54 @@
+import express from 'express'
+import { env } from '~/config/environment'
+import { errorHandlingMiddleware } from '~/middlewares/errorHandlingMiddleware'
+import { connectDB } from '~/config/database'
+
+// router
+import authRoutes from '~/routes/authRoutes'
+import customerRoutes from '~/routes/customerRoutes'
+
+// admin
+import adminRoutes from './routes/adminRoutes'
+
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+const morgan = require('morgan')
+import http from 'http'
+
+
+const START_SERVER = () => {
+  const app = express()
+
+  app.use(express.json())
+  app.use(morgan('dev'))
+  app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+  }))
+  app.use(cookieParser())
+
+  // user router
+  app.use('/api/auth', authRoutes)
+  app.use('/api/customer', customerRoutes)
+  app.use('/api/admin', adminRoutes )
+
+  app.use(errorHandlingMiddleware)
+
+  const server = http.createServer(app)
+
+  server.listen(env.APP_PORT, env.APP_HOST, () => {
+    console.log(`Hello ${env.AUTHOR}, I am running at http://${env.APP_HOST}:${env.APP_PORT}/`)
+  })
+}
+
+(async () => {
+  try {
+    console.log('1. Connecting to MongoDB Cloud Atlas')
+    await connectDB()
+    console.log('2. Connected to MongoDB Cloud Atlas')
+    START_SERVER()
+  } catch (error) {
+    console.error(error)
+    process.exit(0)
+  }
+})()
